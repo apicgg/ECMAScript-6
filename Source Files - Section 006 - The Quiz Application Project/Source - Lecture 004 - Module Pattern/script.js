f74bd58b26b815799459894a4cc62d5a0b3d3c3b
Anurag Pramanik <p.anurag@outlook.com>
2021-12-07 22:22:44 +0530

    Some changes on script.js

--- Source Files - Section 006 - The Quiz Application Project/Source - Lecture 004 - Module Pattern/script.js
+++ Source Files - Section 006 - The Quiz Application Project/Source - Lecture 004 - Module Pattern/script.js
@@ -1,51 +1,32 @@
 // Lecture: Module Pattern
 
-/*
-var quizController = (function() {
+var quizController = (function () {
+  var private = 10;
 
-    var private = 10;
+  var privateFn = function (a) {
+    return a + private;
+  };
 
-    var privateFn = function(a) {
-        return a + private;
-    }
-
-    return {
-        publicMethod: function() {
-            return privateFn(20);
-        }
-    }
-
+  return {
+    publicMethod: function () {
+      return privateFn(20);
+    },
+  };
 })();
 
-console.log(quizController.publicMethod());
+// console.log(quizController.publicMethod());
 
+var UIController = (function () {
+  var num1 = 30;
 
-var UIController = (function() {
-
-    var num1 = 30;
-
-    return {
-        sum: function(num2) {
-            return num1 + num2;
-        }
-    }
-
+  return {
+    sum: function (num2) {
+      return num1 + num2;
+    },
+  };
 })();
 
-
-var controller = (function(quizCtrl, UICtrl) {
-
-    console.log(UICtrl.sum(100) + quizCtrl.publicMethod());
-
+var controller = (function (quizCtrl, UICtrl) {
+  console.log(UICtrl.sum(100) + quizCtrl.publicMethod());
+  // UICtrl.sum(100) + quizCtrl.publicMethod();
 })(quizController, UIController);
-*/
-
-
-
-
-
-
-
-
-
-
